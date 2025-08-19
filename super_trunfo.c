3d4acdcc4e28efd1f428e3efea7ea2c1d5c4349c
Marcelo7717 <58618363+Marcelo7717@users.noreply.github.com>
2025-08-19 14:50:47 -0300

    Update super_trunfo.c
    Desafio nível novato

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    // DADOS DA CARTA 1
+    char estado1;
+    char codigo1[4];
+    char nomecidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+
+    // DADOS DA CARTA 2
+    char estado2;
+    char codigo2[4];
+    char nomecidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
